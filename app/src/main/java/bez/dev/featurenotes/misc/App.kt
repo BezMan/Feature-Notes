a163f190d2d941c564453dc69985e20d43edd5f3
Betz <silverbez@gmail.com>
2019-06-14 00:13:11 +0300

    App class cleanup. SharedPrefs class

--- app/src/main/java/bez/dev/featurenotes/misc/App.kt
+++ app/src/main/java/bez/dev/featurenotes/misc/App.kt
@@ -3,7 +3,6 @@ package bez.dev.featurenotes.misc
 import android.app.Application
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import bez.dev.featurenotes.data.NoteDatabase
 import bez.dev.featurenotes.services.OnClearFromRecentService
 
@@ -13,14 +12,11 @@ class App : Application() {
         super.onCreate()
         appContext = applicationContext
         database = NoteDatabase.getInstance(appContext)
-        prefs = getSharedPreferences(PREFS, MODE_PRIVATE)
 
         startService(Intent(baseContext, OnClearFromRecentService::class.java))
     }
 
     companion object {
-        private const val PREFS = "PREFS"
-        lateinit var prefs: SharedPreferences
         lateinit var appContext: Context
         lateinit var database: NoteDatabase
     }
