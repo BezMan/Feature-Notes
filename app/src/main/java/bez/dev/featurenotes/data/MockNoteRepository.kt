94a9ed36d27986efa78a000752fe746e87f6ac41
Betz <silverbez@gmail.com>
2019-07-15 17:48:39 +0300

    init

--- app/src/main/java/bez/dev/featurenotes/data/MockNoteRepository.kt
+++ app/src/main/java/bez/dev/featurenotes/data/MockNoteRepository.kt
@@ -1,5 +1,8 @@
 package bez.dev.featurenotes.data
 
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
 class MockNoteRepository : NoteRepository() {
 
     init {
@@ -11,8 +14,12 @@ class MockNoteRepository : NoteRepository() {
         if (SharedPrefs.getBoolValue(KEY_FIRST_RUN, true)) {
             clearAllData()
             insert(Note("mock1", 1, "[\"click edit\",\"drag and drop\"]"))
-            insert(Note("mock2", 2, "[\"click edit\",\"drag and drop\"]"))
-            insert(Note("mock3", 3, "[\"click edit\",\"drag and drop\"]"))
+//                    insert(Note("mock2", 2, "[\"click edit\",\"drag and drop\"]"))
+//                    insert(Note("mock3", 3, "[\"click edit\",\"drag and drop\"]"))
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe()
+
             //toggle to not first run anymore:
             SharedPrefs.setBoolValue(KEY_FIRST_RUN, false)
         }
--- app/src/main/java/bez/dev/featurenotes/data/RealNoteRepository.kt
+++ app/src/main/java/bez/dev/featurenotes/data/RealNoteRepository.kt
@@ -1,5 +1,8 @@
 package bez.dev.featurenotes.data
 
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
 class RealNoteRepository : NoteRepository() {
 
     init {
@@ -10,6 +13,10 @@ class RealNoteRepository : NoteRepository() {
         if (SharedPrefs.getBoolValue(KEY_FIRST_RUN, true)) {
             clearAllData()
             insert(Note("swipe to delete", 1, "[\"click edit\",\"drag and drop\"]"))
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe()
+
             //toggle to not first run anymore:
             SharedPrefs.setBoolValue(KEY_FIRST_RUN, false)
         }
