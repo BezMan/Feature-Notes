e30263bb63599128e1588b2a9b9ef9940cb68de7
Betz <silverbez@gmail.com>
2019-11-04 20:13:03 +0200

    gradle update libs and version

--- app/src/main/java/bez/dev/featurenotes/data/RealNoteRepository.kt
+++ app/src/main/java/bez/dev/featurenotes/data/RealNoteRepository.kt
@@ -5,7 +5,7 @@ class RealNoteRepository : NoteRepository() {
     override suspend fun setInitNotes() {
         if (SharedPrefs.getBoolValue(KEY_FIRST_RUN, true)) {
             clearAllData()
-            insert(Note("swipe to delete", 1, "[\"click edit\",\"drag and drop\"]"))
+            insert(Note("This is a note", 1, "[\"item 1\",\"item 2\"]"))
 
             //toggle to not first run anymore:
             SharedPrefs.setBoolValue(KEY_FIRST_RUN, false)
