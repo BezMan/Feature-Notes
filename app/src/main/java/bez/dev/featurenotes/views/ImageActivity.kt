1fd20ca6c66334eebc3d0e701c21b014d725ac30
Betz <silverbez@gmail.com>
2020-06-06 17:54:20 +0300

    copy text logic

--- app/src/main/java/bez/dev/featurenotes/views/ImageActivity.kt
+++ app/src/main/java/bez/dev/featurenotes/views/ImageActivity.kt
@@ -1,53 +1,52 @@
 package bez.dev.featurenotes.views
 
 import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import bez.dev.featurenotes.R
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.text.FirebaseVisionText
+import kotlinx.android.synthetic.main.activity_image.*
 
 class ImageActivity : AppCompatActivity() {
+
+    companion object{
+        const val REQUEST_IMAGE = 123
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
-
-        imageView = findViewById(R.id.imageToText_image)
-        editText = findViewById(R.id.imageToText_text)
     }
-
-
-    lateinit var imageView: ImageView
-    lateinit var editText: EditText
 
 
     fun selectImage(v: View) {
         val intent = Intent()
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_IMAGE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            imageView.setImageURI(data!!.data)
+        if (requestCode == REQUEST_IMAGE && resultCode == Activity.RESULT_OK) {
+            imageToText_image.setImageURI(data!!.data)
             startRecognizing()
         }
     }
 
     private fun startRecognizing() {
-        if (imageView.drawable != null) {
-            editText.setText("")
-            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        if (imageToText_image.drawable != null) {
+            imageToText_text.setText("")
+            val bitmap = (imageToText_image.drawable as BitmapDrawable).bitmap
             val image = FirebaseVisionImage.fromBitmap(bitmap)
             val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
 
@@ -56,7 +55,7 @@ class ImageActivity : AppCompatActivity() {
                         processResultText(firebaseVisionText)
                     }
                     .addOnFailureListener {
-                        editText.setText("Failed")
+                        imageToText_text.setText("Failed")
                     }
         } else {
             Toast.makeText(this, "Select an Image First", Toast.LENGTH_LONG).show()
@@ -67,13 +66,28 @@ class ImageActivity : AppCompatActivity() {
 
     private fun processResultText(resultText: FirebaseVisionText) {
         if (resultText.textBlocks.size == 0) {
-            editText.setText("No Text Found")
+            imageToText_text.setText("No Text Found")
             return
         }
         for (block in resultText.textBlocks) {
             val blockText = block.text
-            editText.append(blockText + "\n")
+            imageToText_text.append(blockText + "\n")
         }
     }
+
+    fun clickCopyText(view: View) {
+        copyTextToClipboard(imageToText_text.text.toString())
+    }
+
+    private fun copyTextToClipboard(text: String) {
+        if (text.trim().isNotEmpty()) {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("item", text)
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(this, "copied to clipboard", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
 }
