3424e5a632a7cf13033f7a157505d989dc477f95
Betz <silverbez@gmail.com>
2020-08-02 23:50:08 +0300

    mid way to fragment separation

--- app/src/main/java/bez/dev/featurenotes/views/ArchiveFragment.kt
+++ app/src/main/java/bez/dev/featurenotes/views/ArchiveFragment.kt
@@ -1,11 +1,37 @@
 package bez.dev.featurenotes.views
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import bez.dev.featurenotes.R
+import bez.dev.featurenotes.data.Note
+import bez.dev.featurenotes.views.BaseActivity.Companion.toggleShowView
+import kotlinx.android.synthetic.main.fragment_archive.*
+import kotlinx.android.synthetic.main.no_notes_layout.*
 
-class ArchiveFragment : Fragment() {
+class ArchiveFragment : Fragment(), ArchiveListAdapter.OnItemClickListener {
+
+    private lateinit var archiveListAdapter: ArchiveListAdapter
+    private var archivedList: List<Note> = ArrayList()
+    private lateinit var restorePoint: List<Note>
+    private lateinit var mContext: Context
+    private lateinit var baseActivity: BaseActivity
+
+    private val observer = Observer<List<Note>> {
+        archivedList = it
+        refreshUI()
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +44,83 @@ class ArchiveFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        baseActivity = activity as BaseActivity
+
+        initNoteViewModel()
+
+        initUI()
+    }
+
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.archive_fragment_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
+
+
+    private fun initUI() {
+        //RECYCLER
+        recycler_view_archive.layoutManager = LinearLayoutManager(context)
+        recycler_view_archive.setHasFixedSize(true)
+        archiveListAdapter = ArchiveListAdapter(this)
+        recycler_view_archive.adapter = archiveListAdapter
+    }
+
+
+    private fun initNoteViewModel() {
+        baseActivity.repoViewModel.allNotes.observe(viewLifecycleOwner, observer)
+    }
+
+
+    private fun refreshUI() {
+        archiveListAdapter.submitList(archivedList)  //reads the adapter DIFF we created, and displays list
+
+        //no notes layout
+        no_notes_view.toggleShowView(archivedList.isNullOrEmpty())
+
+        //Notification
+        baseActivity.notificationManager.updateNotification(archivedList)
+    }
+
+
+    override fun onNoteItemTextClick(note: Note) {
+        baseActivity.editNote(note)
+    }
+
+    override fun onNoteItemOverflowClick(note: Note, overflow: ImageView, noteHolder: ArchiveListAdapter.NoteHolder) {
+        val popup = PopupMenu(mContext, overflow)
+        //Inflating the Popup using xml file
+        popup.menuInflater.inflate(R.menu.overflow_note_popup, popup.menu)
+
+        //registering popup with OnMenuItemClickListener
+        popup.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.main_overflow_note_edit -> {
+                    baseActivity.editNote(note)
+                }
+                R.id.main_overflow_note_share -> {
+                    baseActivity.shareNote(note)
+                }
+//                R.id.main_overflow_note_delete -> {
+//                    baseActivity.deleteNote(note)
+//                    showUndoDelete(note)
+//                }
+//                R.id.main_overflow_note_archive -> {
+//                    baseActivity.archiveNote(note)
+//                    showUndoArchive(note)
+//                }
+
+            }
+            false
+        }
+
+        popup.show() //showing popup menu
+    }
+
+
 
 
     companion object {
---
+++ app/src/main/java/bez/dev/featurenotes/views/ArchiveListAdapter.kt
@@ -0,0 +1,85 @@
+package bez.dev.featurenotes.views
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import bez.dev.featurenotes.R
+import bez.dev.featurenotes.data.Note
+import kotlinx.android.synthetic.main.main_activity_list_item.view.*
+
+class ArchiveListAdapter internal constructor(context: OnItemClickListener) : ListAdapter<Note, ArchiveListAdapter.NoteHolder>(DIFF_CALLBACK) {
+    private var listener: OnItemClickListener = context
+
+    internal fun getNoteAt(position: Int): Note {
+        return getItem(position)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): NoteHolder {
+        val itemView = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.archive_list_item, viewGroup, false)
+        return NoteHolder(itemView)
+    }
+
+
+    //methods preventing click on an item affecting different items//
+    override fun getItemViewType(position: Int): Int {
+        return getNoteAt(position).id.toInt()
+    }
+
+    override fun getItemId(position: Int): Long {
+        return getNoteAt(position).id
+    }
+    //methods preventing click on an item affecting different items//
+
+
+    override fun onBindViewHolder(noteHolder: NoteHolder, position: Int) {
+        val currentNote = getNoteAt(position)
+        noteHolder.tvTitle.text = currentNote.title
+        noteHolder.tvPriority.text = currentNote.priority.toString()
+
+        noteHolder.tvTitle.setOnClickListener {
+            listener.onNoteItemTextClick(currentNote)
+        }
+        noteHolder.overflow.setOnClickListener {
+            listener.onNoteItemOverflowClick(currentNote, noteHolder.overflow, noteHolder)
+        }
+    }
+
+
+    interface OnItemClickListener {
+        fun onNoteItemTextClick(note: Note)
+        fun onNoteItemOverflowClick(note: Note, overflow: ImageView, noteHolder: NoteHolder)
+    }
+
+    inner class NoteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal val tvTitle: TextView = itemView.text_view_title
+        internal val tvPriority: TextView = itemView.text_view_priority
+        internal val overflow: ImageView = itemView.overflow_image_note_item
+    }
+
+    companion object {
+
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Note>() {
+            override fun areItemsTheSame(oldNote: Note, newNote: Note): Boolean {
+                return oldNote.id == newNote.id
+            }
+
+            override fun areContentsTheSame(oldNote: Note, newNote: Note): Boolean {
+                return oldNote.title == newNote.title
+                        && oldNote.items == newNote.items
+                        && oldNote.priority == newNote.priority
+                        && oldNote.isNotification == newNote.isNotification
+                        && oldNote.color == newNote.color
+                        && oldNote.numItems == newNote.numItems
+                        && oldNote.isArchived == newNote.isArchived
+                        && oldNote.timeCreated == newNote.timeCreated
+                        && oldNote.timeModified == newNote.timeModified
+            }
+        }
+    }
+}
