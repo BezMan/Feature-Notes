a213de975311be7d13dcd8931e37ecaa608bb777
Betz <silverbez@gmail.com>
2023-07-31 12:26:04 +0300

    new gradle structure

--- app/src/main/java/bez/dev/featurenotes/data/Note.kt
+++ app/src/main/java/bez/dev/featurenotes/data/Note.kt
@@ -4,7 +4,7 @@ package bez.dev.featurenotes.data
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @Entity(tableName = "note_table")
